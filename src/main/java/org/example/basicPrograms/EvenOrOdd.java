2d892c3e4c2b54dc734fce2e1e16bbea740e7b94
Navitha214 <navithaadhoni2114@gmail.com>
2026-05-06 14:52:33 +0530

    ebox

--- src/main/java/org/example/basicPrograms/EvenOrOdd.java
+++ src/main/java/org/example/basicPrograms/EvenOrOdd.java
@@ -1,4 +1,19 @@
 package org.example.basicPrograms;
 
+import java.util.Scanner;
+
 public class EvenOrOdd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int i = sc.nextInt();
+
+        if(i%2==0){
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd number");
+        }
+
+    }
+
 }
--- src/main/java/org/example/basicPrograms/ReverseNumber.java
+++ src/main/java/org/example/basicPrograms/ReverseNumber.java
@@ -1,4 +1,5 @@
 package org.example.basicPrograms;
 
 public class ReverseNumber {
+
 }
